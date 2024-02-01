ec2793c68015eeea5b357732cb4b2665df500698
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-02-01 20:54:30 -0300

    feat: creating Products MVC

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -19,12 +19,12 @@ export class ProductsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+    return this.productsService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
