b0adbec35d3f85857a0d4aeeb1959b875e673ccf
Henrique Almeida - Frontall USA / BON <halmeida.bon@frontallusa.com>
2024-02-01 21:12:50 -0300

    feat: creating order dto

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,24 @@
-export class Order {}
+export enum OrderStatus {
+    PENDING = 'pending',
+    PAID = 'paid',
+    FAILED = 'failed',
+}
+
+export class Order {
+    id: string;
+
+    total: number;
+
+    client_id: number;
+
+    status: OrderStatus = OrderStatus.PENDING;
+
+    created_at: Date;
+}
+
+
+export class OrderItem {
+    id: number;
+
+    
+}
